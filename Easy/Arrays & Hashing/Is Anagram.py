9f2229b8329695d955e3716a9e215a94f2c84ab7
Rahul Nagarajan <rahul.nagarajan@peopletech.com>
2024-11-12 15:25:55 -0800

    code cleanup

--- Easy/Arrays & Hashing/Is Anagram.py
+++ Easy/Arrays & Hashing/Is Anagram.py
@@ -1,29 +1,29 @@
+#Naive
 class Solution:
-        #Naive
-        class Solution:
-            def isAnagram(self, s: str, t: str) -> bool:
-                if len(s) != len(t):
-                    return False
-                return sorted(s) == sorted(t)
-        #Optimal    
-        class Solution:
-            def isAnagram(self, s: str, t: str) -> bool:
-                if len(s) != len(t):
-                    return False
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
 
-                hmap_s = {}
-                for i in s:
-                    if i not in hmap_s:
-                        hmap_s[i] = 1
-                    else:
-                        hmap_s[i] += 1 
+#Optimal    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
-                for j in t: 
-                    if j in hmap_s:
-                        hmap_s[j] -= 1 
-                        if hmap_s[j] < 0: 
-                            return False 
-                    else:
-                        return False 
-                
-                return True 
+        hmap_s = {}
+        for i in s:
+            if i not in hmap_s:
+                hmap_s[i] = 1
+            else:
+                hmap_s[i] += 1 
+
+        for j in t: 
+            if j in hmap_s:
+                hmap_s[j] -= 1 
+                if hmap_s[j] < 0: 
+                    return False 
+            else:
+                return False 
+        
+        return True 
