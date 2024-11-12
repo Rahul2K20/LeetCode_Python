0692d055c85b56bd87925f8ed442defa7e14c33b
Rahul Nagarajan <rahul.nagarajan@peopletech.com>
2024-11-12 15:24:23 -0800

    typo fix in Is Anagram.py

--- Is Anagram.py
+++ Is Anagram.py
@@ -1,14 +1,10 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        
         #Naive
-        
         class Solution:
             def isAnagram(self, s: str, t: str) -> bool:
                 if len(s) != len(t):
                     return False
                 return sorted(s) == sorted(t)
-
         #Optimal    
         class Solution:
             def isAnagram(self, s: str, t: str) -> bool:
